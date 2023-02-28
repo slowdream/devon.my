0d29759414788366ece349609d39e9999736c499
gamov.av <avg@technodoc.io>
2023-03-01 00:52:06 +0300

    auto fix & lint

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -3,14 +3,14 @@
  * to our Laravel back-end. This library automatically handles sending the
  * CSRF token as a header based on the value of the "XSRF" token cookie.
  */
-import axios from "axios";
-import _ from "lodash";
+import axios from 'axios';
+import _ from 'lodash';
 
 window._ = _;
 
 window.axios = axios;
 
-window.axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
+window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
